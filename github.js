c69b032abc5a8bd66c3252c5ad9a2659920d8d1e
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-31 16:08:17 +1300

    create function to get user from the GitHub API ands return it to be used by the app

--- github.js
+++ github.js
@@ -3,4 +3,12 @@ class GitHub {
     this.client_id = "051a57a7986cac3a8693";
     this.client_secret = "cfdc6c530142bcb9d54b110175e760ce3e6c96d9";
   }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+    const profileData = await profileResponse.json();
+    return { profileData };
+  }
 }
