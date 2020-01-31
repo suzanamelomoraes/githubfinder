de02e1e0714f1b0ea5f6b1d5fcb5191821ac629d
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-31 17:34:02 +1300

    call ui class method called showAlert to show an alert when the input given does not exist

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ searchUser.addEventListener("keyup", e => {
   if (userText !== "") {
     github.getUser(userText).then(data => {
       if (data.profile.message === "Not Found") {
-        //Show alert
+        ui.showAlert("User not found", "alert alert-danger");
       } else {
-        //show profile
         ui.showProfile(data.profile);
       }
     });
