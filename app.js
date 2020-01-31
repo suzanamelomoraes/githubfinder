90cd450f998a356252f2532401af1c8c20e32987
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-31 16:32:40 +1300

    instantiate Github class to receive data from github API

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+const github = new Github();
+
 const searchUser = document.getElementById("searchUser");
 
 searchUser.addEventListener("keyup", e => {
   const userText = e.target.value;
 
   if (userText !== "") {
+    github.getUser(userText).then(data => {
+      console.log(data);
+    });
   }
 });
