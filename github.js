23f51438edcce5186bb31ed9bf1d1d9e74a21199
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-02-02 15:50:08 +1300

    create variable to fetch the API with repos data up to 5 lastest user repos

--- github.js
+++ github.js
@@ -2,13 +2,25 @@ class Github {
   constructor() {
     this.client_id = "051a57a7986cac3a8693";
     this.client_secret = "cfdc6c530142bcb9d54b110175e760ce3e6c96d9";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
     const profileResponse = await fetch(
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
+
+    const reposResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
     const profile = await profileResponse.json();
-    return { profile };
+    const repos = await reposResponse.json();
+
+    return {
+      profile,
+      repos
+    };
   }
 }
