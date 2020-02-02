fc453cfcfa3dc2e97d0832aa29651e8dc18ad425
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-02-02 16:16:01 +1300

    removed from the code client id and secret numbers given by github to unlimited number of access per day to the api, without those codes the number of access is limited to 100

--- github.js
+++ github.js
@@ -1,15 +1,18 @@
 class Github {
   constructor() {
-    this.client_id = "051a57a7986cac3a8693";
-    this.client_secret = "cfdc6c530142bcb9d54b110175e760ce3e6c96d9";
+    // this.client_id = "Here goes the Client ID number given by GitHub";
+    // this.client_secret = "Here goes the Client Secret number given by GitHub";
     this.repos_count = 5;
     this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
-    const profileResponse = await fetch(
-      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
-    );
+    //complete fetch code including client ID and Secret given by GitHub to the user to unlimited number of access per day
+    // const profileResponse = await fetch(
+    //   `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    // );
+
+    const profileResponse = await fetch(`https://api.github.com/users/${user}`);
 
     const reposResponse = await fetch(
       `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
